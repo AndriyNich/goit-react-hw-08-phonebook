305f09cb92fa94390941cba341a7cf07eaa803d0
AndriyNich <111660438+AndriyNich@users.noreply.github.com>
2023-05-22 16:32:34 +0300

    feature:delete
    function deleting contact from list contacts

--- src/components/Contacts/TableContacts/TableCellActions/TableCellActions.jsx
+++ src/components/Contacts/TableContacts/TableCellActions/TableCellActions.jsx
@@ -5,8 +5,16 @@ import TableCell from '@mui/material/TableCell';
 import IconButton from '@mui/material/IconButton';
 import DeleteForeverIcon from '@mui/icons-material/DeleteForever';
 import EditIcon from '@mui/icons-material/Edit';
+import { useDispatch } from 'react-redux';
+import { deleteContact } from 'redux/contacts/operation';
 
 export function TableCellActions({ id }) {
+  const dispatch = useDispatch();
+
+  const handleDeleteContact = () => {
+    dispatch(deleteContact(id));
+  };
+
   return (
     <TableCell align="center">
       <IconButton
@@ -17,12 +25,7 @@ export function TableCellActions({ id }) {
       >
         <EditIcon />
       </IconButton>
-      <IconButton
-        aria-label="delete"
-        onClick={() => {
-          console.log(`delete ${id}`);
-        }}
-      >
+      <IconButton aria-label="delete" onClick={handleDeleteContact}>
         <DeleteForeverIcon />
       </IconButton>
     </TableCell>
